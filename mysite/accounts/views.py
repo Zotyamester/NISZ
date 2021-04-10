0ba41a38c3b79e7f0b950c24e0e17cd7b57861c1
Zotyamester <szatmary.zoltan1222@gmail.com>
2021-04-10 11:50:21 +0200

    Autentikációs rendszer

---
+++ mysite/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Your account has been created.')
+            login(request, user)
+            return redirect('main:home')
+    return render(request, 'registration/register.html', {'form': form})
