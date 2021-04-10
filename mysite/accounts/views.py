e634d6a7589dc04ad8c8da535b61601bcb05dba3
Zotyamester <szatmary.zoltan1222@gmail.com>
2021-04-10 14:12:08 +0200

    Profil cuccos

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -2,17 +2,42 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+
+@login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(
+                request, 'A kért módosítások sikeresen végrehajtódtak.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'accounts/profile.html', context)
+
 
 def register(request):
-    form = UserCreationForm(request.POST or None)
+    if request.user.is_authenticated:
+        return redirect('main:home')
     if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            messages.success(request, 'A fiók létrejött.')
-            login(request, user)
-            return redirect('main:home')
+            form.save()
+            messages.success(request, 'A fiók létrejött')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
     return render(request, 'registration/register.html', {'form': form})
