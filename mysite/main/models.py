a9f992c3dc56d5b45ffe6acebb754b854fd3ef3b
SzekiX <2003szekeres.marci@gmail.com>
2021-04-10 14:14:30 +0200

    Topic nev kiiras

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     name = models.CharField(max_length=30)
 
+        def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
