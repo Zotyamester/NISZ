f46e87b3edf2f93f59a1de3828e26adbdcf32903
Zotyamester <szatmary.zoltan1222@gmail.com>
2021-04-11 05:58:47 +0200

    Bugfix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,14 +78,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nisz',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': 'c10sqldb',
+        'USER': 'c10sql',
+        'PASSWORD': 'eyycVqCS!V98',
+        'HOST': 'versenydonto.nisz.hu',
         'PORT': '3306',
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
