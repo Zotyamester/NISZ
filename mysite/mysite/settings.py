06acf7eab863e42455a87a53c3f2040f75c6cb14
Zotyamester <szatmary.zoltan1222@gmail.com>
2021-04-11 06:31:18 +0200

    Vissza sqlite3-ra

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'c10sqldb',
-        'USER': 'c10sql',
-        'PASSWORD': 'eyycVqCS!V98',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
